28e7eacf238ebce8ec57060c5e74556fc7ba2b63
Matthew Hokanson <m@h0ke.com>
2014-03-06 14:59:36 -0800

    rename from ln to preact. use json instead of form data. general clean up.

--- ln.py
+++
@@ -1,6 +0,0 @@
-import config, requests
-
-def log_event(data):
-  url = config.LN_API_URL + config.LN_EVENTS_ENDPOINT
-  resp = requests.post(url, data=data, auth=(config.LN_PROJECT_CODE, config.LN_API_SECRET))
-  return resp
